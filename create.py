3efe2f30be06094aeabb1608c5537fb066f88d86
Vinícius Alves Pires <viniccius.alves.pires@gmail.com>
2025-06-25 00:39:29 -0300

    Initial commit

---
+++ create.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+with open("impar-ou-par.py", "a") as myfile:
+    myfile.write('''
+numero = int(input("Digite um número: "))
+    ''')
+
+def generate(r1 , r2):
+    for i in range(r1 , r2):
+        sys.stdout.write(f"\rIterations: {i}    Bytes: {os.path.getsize('./impar-ou-par.py')}    ")
+        sys.stdout.flush()
+        string = ''
+        if i % 2 == 0:
+            string = 'par'
+        else:
+            string = 'ímpar'
+
+        text = '''
+if numero == {i}: 
+    print("O número {i} é {string}.")
+        '''.format(i=i, string=string)
+
+
+        with open("impar-ou-par.py", "a") as myfile:
+            myfile.write(text)
+
+mode = int(input("1 para breakpoint, 2 para tamanho em esperado: "))
+
+
+if mode == 1:
+    breakpoint = int(input("Digite o breakpoint: "))
+    generate(0 ,breakpoint)
+    
+elif mode == 2:
+    targetBytes = int(input("Digite o tamanho esperado: "))
+
+    atualBytes = os.path.getsize('./impar-ou-par.py')
+
+    i = 0
+
+    while atualBytes < targetBytes:
+        generate(i , i +1 )
+        i += 1
+        atualBytes = os.path.getsize('./impar-ou-par.py')
+
